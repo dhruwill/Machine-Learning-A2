18b241d2cddf9fb3c2033d8bbd9a8712f1f12e07
YugAjmera <yugajmera@gmail.com>
2020-04-06 00:03:18 +0530

    logistic regression with regularization added

--- LR_3.py
+++ LR_3.py
@@ -26,6 +26,17 @@ def accuracy(y_pred, test):
 			not_acc = not_acc + 1
 	return (float(acc)/float(acc + not_acc))
 
+def stan(X):
+	mean = np.mean(X, axis=0)			# axis = 0 for along the column
+	std = np.std(X, axis=0, ddof=1)     #ddof = 1 for sample standard deviation
+ 	
+ 	X = X.astype(float)
+
+	for i in range(0, X.shape[0]):
+		for j in range(0, X.shape[1]):
+			X[i][j] = (float(X[i][j] - mean[j])/float(std[j]))
+
+	return X
 
 
 if __name__ == "__main__":
@@ -37,8 +48,12 @@ if __name__ == "__main__":
 
 	X_train, X_test, T_train, T_test = train_test_split(X, T, test_size=0.2)
 
-	epoch = 500
+	X_train_standard = stan(X_train)
+	X_test_standard = stan(X_test)
+	
+	epoch = 5000
 	lr = 0.5
+	L2_coeff = 0.0001
 	np.random.seed(0)
 	W = np.random.uniform(0,1,size=(X_train.shape[1],1)) 	#intial weights
 	b = 1                                        			    #bias
@@ -51,16 +66,15 @@ if __name__ == "__main__":
 		grad = y_predicted - T_train
 		gradient= np.dot(np.transpose(X_train),grad)/(X_train.shape[0])
 		grad_bias = np.average(grad)
-		W = W - lr*gradient
-		b = b - lr*grad_bias
-	
+		W = W - lr*gradient + L2_coeff*2*(W)
+		b = b - lr*grad_bias + L2_coeff*2*(b)
+
 	print(W)
+	print(b)
 
 	T_pred = predict(X_test, W, b)
 	a = accuracy(T_pred, T_test)
 	print("accuracy",a)
-
-
 	
 
 	
